410fab0f27aa69a559322979ca38854c1cfca826
BaddelaManikanta <121152589+BaddelaManikanta@users.noreply.github.com>
2023-03-06 20:47:03 +0530

    Update views.py

--- snap/views.py
+++ snap/views.py
@@ -3,10 +3,3 @@ from django.http import HttpResponse
 # Create your views here.
 def snapchat(request):
     return HttpResponse('<h1><marquee>Hi Users Now We Are Using SnapChat</marquee></h1>')
-
-
-def add(request):
-    n1=int(input('Enter First Number : '))
-    n2=int(input('Enter Second Number : '))
-    res=n1+n2
-    return HttpResponse(f'Result is = {res} !!!!!!!')
